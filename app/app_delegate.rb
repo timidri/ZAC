e6f2b1b0dcff40f8acc402ebfd7bdfe808a1897d
Dimitri Tischenko <dimitri@proteon.nl>
2012-05-17 00:41:41 +0200

    Now using a storyboard

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = NSBundle.mainBundle.loadNibNamed('ZAC', owner:self, options:nil).first
+    # @window.rootViewController = NSBundle.mainBundle.loadNibNamed('ZAC', owner:self, options:nil).first
+    storyboard = UIStoryboard.storyboardWithName("ZAC", bundle:nil)
+    rootViewController = storyboard.instantiateViewControllerWithIdentifier("Main")
+    @window.rootViewController = rootViewController
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
     true
