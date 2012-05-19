65f0ad2f474aa2c9c89e5096f3f118c37f29edaf
Dimitri Tischenko <dimitri@proteon.nl>
2012-05-19 15:54:43 +0200

    Filtering games works

--- app/zac_games_controller.rb
+++ app/zac_games_controller.rb
@@ -8,29 +8,39 @@ class ZACGamesController < UITableViewController
     # puts("#{self.class} viewDidLoad")
     @dateFormatter = NSDateFormatter.alloc.init.setDateFormat("E dd-MM-yyyy HH:mm")
     @switch = self.view.viewWithTag(SWITCH_TAG)
-    puts("switch: #{@switch}")
     @switch.addTarget(self, action:'switchClicked:', forControlEvents:UIControlEventValueChanged)
+    @currentSelection = SHOW_UPCOMING_GAMES
   end
 
   def switchClicked(sender)
-    puts("switch changed to #{sender.selectedSegmentIndex}")
-    p sender
+    # puts("switch changed to #{sender.selectedSegmentIndex}")
+    @currentSelection = sender.selectedSegmentIndex
+    updateGamesSelection(@currentSelection)
+  end
+  
+  def updateGamesSelection(currentSelection)
+    if @currentSelection == SHOW_ALL_GAMES
+      @games = @team.games
+    else
+      @games = @team.upcomingGames
+    end
+    self.view.reloadData
   end
   
   def teamname= teamname
     @team = Team.find_by_name teamname
     navigationItem.title = "Wedstrijden: #{teamname}"
+    updateGamesSelection(@currentSelection)
   end
   
   def tableView(tableView, numberOfRowsInSection:section)
-    # puts "game count: #{@team.games.count}"
-    @team.upcomingGames.count
+    @games.count
   end
   
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
     # puts("cellForRowAtIndexPath: #{indexPath.row}")
     cell = tableView.dequeueReusableCellWithIdentifier("GameCell")
-    game = @team.upcomingGames[indexPath.row]
+    game = @games[indexPath.row]
     cell.textLabel.text = "#{@dateFormatter.stringFromDate(game.datetime)}"
     cell.detailTextLabel.text = "tegen: #{game.opponentOf(@team)} veld: #{game.field}"
     cell
